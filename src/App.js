2aeaa18c413d7e367e5173b2e85f7ac53f855fcf
phamthaihoang1234 <70738519+phamthaihoang1234@users.noreply.github.com>
2021-04-14 11:29:53 +0700

    form-reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+        username : ''
+    };
+  }
+
+  onHandleChange = (event) => {
+    this.setState({
+      username : event.target.value
+    })
+
+  }
+
+  onHandleSubmit = (event) =>{
+    event.preventDefault();// chặn onsubmit dữ liệu
+  }
+
+
+
+
+  render(){
+    return (
+        <div className="container mt-30">
+          <div className="row">
+            <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+              <div className="panel panel-primary">
+                <div className="panel-heading">
+                  <h3 className="panel-title">Form</h3>
+                </div>
+                <div className="panel-body">
+                  <form onSubmit= {this.onHandleSubmit} >
+                    <div className="form-group">
+                      <label >Username</label>
+                      <input type="text"
+                             className="form-control"
+                             name="txtName"
+                             onChange={this.onHandleChange}
+                      />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Save</button>
+                    <button type="reset" className="btn btn-default">Delete</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    );
+  }
 }
+
 
 export default App;
